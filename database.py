b530b3de68a3974de230219ec2a4889de6baa138
Eric Mignardi <mignardi.e@gmail.com>
2024-10-22 13:32:19 -0400

    Added requirements.txt

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine("mysql+mysqlconnector://root:password@localhost:3306/Stratolog")
+engine = create_engine("mysql+mysqlconnector://root:password@0.0.0.0:3306/Stratolog")
 
 Base = declarative_base()
 
